766f48aed506f945d2147e6f02a022d168a1b95d
Allegra <aukseallegra@gmail.com>
2018-10-22 22:22:46 +0100

    message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import polarBear from '../images/polar_bear.jpg'
 
 import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hi Rimvydas!</h1>
+    <p>Welcome to my new Gatsby site.</p>
+    <p>Which I just deployed on Netlify.</p>
+    <Link to="/page-2/">Click here</Link>
+    <img src={polarBear} alt=""></img>
   </Layout>
 )
 
